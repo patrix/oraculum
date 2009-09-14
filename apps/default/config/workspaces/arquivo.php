e8434a6400d97b550519af16732bdfd1f2918bb8
Patrick Kaminski <patrix.sbs@gmail.com>
2009-09-14 19:56:33 -0300

    Atualizado o núcleo do framework :: Updated the core of the framework

--- apps/default/config/workspaces/arquivo.php
+++ apps/default/config/workspaces/arquivo.php
@@ -3,6 +3,7 @@
   define("IP_INT", "127.0.0.1");
   ini_set("display_errors", false);
   define("DEBUG", 0); // Debug
+  define("PROFILER", 0); // Profiler
   error_reporting(E_ALL|E_STRICT); // Reportando todos os erros e alertas (STRICT)
   define("URL", "http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME'])."/"); // URL Absoluta do sistema
   define("BASE", count(explode("/", URL))-3); // Numeros de diretorios na URL
